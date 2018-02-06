0f111b5aae430bec403f3ea2caf231dd9a504da1
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-02-06 14:06:52 +0100

    Adds range selection to annotation dump. Updates .gitignore (anndump).

--- src/ann_dump.c
+++ src/ann_dump.c
@@ -2,23 +2,28 @@
 
 int main(int argc, char *argv[])
 {
-   if (argc != 2) {
+   if (argc != 4) {
       fprintf(stderr, "usage: %s index-file\n", argv[0]);
    }
 
+   int beg = atoi(argv[1]);
+   int end = atoi(argv[2]);
+
    // Load index.
-   index_t * index = index_load_base(argv[1]);
+   index_t * index = index_load_base(argv[3]);
    if (index == NULL) {
       fprintf(stderr, "error loading index.\n");
       exit(1);
    }
 
    // Load annotations.
-   annlist_t * anns = ann_index_read(argv[1]);
+   annlist_t * anns = ann_index_read(argv[3]);
    if (anns == NULL) {
       fprintf(stderr, "error loading annotations\n");
       exit(1);
    }
+
+   fprintf(stderr,"Read %d annotations.\n", anns->count);
 
    // Dump annotations.
    for (int i = 0; i < anns->count; i++) {
@@ -26,7 +31,7 @@ int main(int argc, char *argv[])
       fprintf(stderr, "Annotation info of: %s\n", ann.file);
       fprintf(stderr, "magic = 0x%llx\nkmer = %d\ntau = %d\nsize = %ld\n", ann.data->magic, ann.data->kmer, ann.data->tau, ann.data->size);
       char * buf = calloc(51,sizeof(char));
-      for (int j = 0; j < ann.data->size; j += 50) {
+      for (int j = max(0,beg); j < min(ann.data->size,end); j += 50) {
          size_t len = min(50, ann.data->size-j-1);
          buf[len] = 0;
          strncpy(buf, index->genome+j, min(50, index->size-j-1));
@@ -34,7 +39,9 @@ int main(int argc, char *argv[])
 
          fprintf(stderr, "cnt  ");
          for (int k = j; k < j+len; k++)
-            fprintf(stderr, "%d", ann.data->data[k] & 0x0F);
+            fprintf(stderr, "%x", ann.data->data[k] & 0x0F);
+
+         
 
          fprintf(stderr, "\ntau  ");
          for (int k = j; k < j+len; k++) {
