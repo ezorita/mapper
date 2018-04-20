be81aecf759a686decd80ea7fcf6a733fe7e44a3
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-04-20 16:19:20 +0200

    Updates test lib to include 'index_txt' tests.

--- test/runtests.c
+++ test/runtests.c
@@ -8,10 +8,12 @@ main(
 {
 
    // Import the test cases from linked files. //
-   extern test_case_t test_cases_from_file_1[];
+   extern test_case_t test_cases_index_sym[];
+   extern test_case_t test_cases_index_txt[];
 
    const test_case_t *test_case_list[] = {
-      test_cases_from_file_1,
+      test_cases_index_sym,
+      test_cases_index_txt,
       NULL, // Sentinel. //
    };
 
